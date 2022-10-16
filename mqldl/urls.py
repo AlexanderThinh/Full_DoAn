0e52cfafddbf34874aba004fcea3f6a1d887bd59
alexanderthinh <alexanderthinh@gmail.com>
2022-10-16 17:13:49 +0700

    Update urls.py

--- mqldl/urls.py
+++ mqldl/urls.py
@@ -38,7 +38,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('', include('qldl.urls')),
     path('admin/', admin.site.urls),
-    re_path('.*', TemplateView.as_view(template_name='index.html'))
+    re_path('.*', TemplateView.as_view(template_name='index.html')),
 
     re_path(r'^swagger(?P<format>\.json|\.yaml)$',
         schema_view.without_ui(cache_timeout=0),
